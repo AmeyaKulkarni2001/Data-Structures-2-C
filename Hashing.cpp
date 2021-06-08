56342ca0c56e6e4b767545377060cde1c648390d
AmeyaKulkarni2001 <71250891+AmeyaKulkarni2001@users.noreply.github.com>
2021-06-08 10:20:42 +0530

    adding function changing display

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,177 +1,207 @@
 #include <iostream>
+#define max 10
 using namespace std;
-int n;
-class Student
+class student
 {
-public:
-    int roll_number;
-    string name;
-    char grade;
-    friend class Hash;
+	int roll_no;
+	string name;
+	char grade;
+	public :
+		student()
+		{
+			roll_no=-1;
+			name="";
+			grade=' ';
+		}
+		void accept()
+		{
+		cout<<"\nEnter the student details\n";
+		cout<<"Roll number: ";
+		cin>>roll_no;
+		cout<<"Name: ";
+		cin>>name;
+		cout<<"Grade: ";
+		cin>>grade;
+		}
+		friend class Hash;
 };
 
 class Hash
 {
-
-    Student HashTable[10],HashTable2[10], temp;
-
-public:
-    Hash();
-    void create();
-    void LinearProb_Without(Student HashTab, int key);
-    void display();
-    void display2();
-    void LinearProb_With(Student HashTab, int key);
+	student Hashtable[max];
+	public :
+		Hash()
+		{
+			for(int i=0;i<max;i++)
+				Hashtable[i].roll_no=-1;
+		}
+		void linear_without(student );
+		void linear_with(student );
+		void search(int);
+		void display();
+		void display_wo();
 };
-void Hash::create()
+
+void Hash :: linear_with(student s)
 {
-    Student s;
-    cout << "\nEnter the Number of Students:";
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << "------------------------------------------------\n";
-        cout << "\nEnter name:";
-        cin >> s.name;
-        cout << "\nEnter Roll Number of Student:";
-        cin >> s.roll_number;
-        cout << "\nEnter Grade of Student:";
-        cin >> s.grade;
-        LinearProb_Without(s, s.roll_number);
-        LinearProb_With(s,s.roll_number);
-    }
+	int loc=s.roll_no%max;
+	if(Hashtable[loc].roll_no==-1)		//if the home address is blank
+	{
+		Hashtable[loc].roll_no=s.roll_no;
+		Hashtable[loc].name=s.name;
+		Hashtable[loc].grade=s.grade;
+		return ;
+	}
+	else
+	{
+	    cout << "\nCOLLISION OCCURRED\n";
+		student temp;
+		temp=s;
+		if(loc!=(Hashtable[loc].roll_no%max))	//if the element already present does not have the home address at this position
+		{
+			temp=Hashtable[loc];
+			Hashtable[loc]=s;
+		}
+		int i=(loc+1)%max;
+		while(i!=loc)
+		{
+			if(Hashtable[i].roll_no==-1)	//place the replaced element at the next blank position in the table
+			{
+				Hashtable[i]=temp;
+				return ;
+			}
+			i=(i+1)%max;
+		}
+		if(i==loc)
+			cout<<"\nHashtable is full\n";
+	}
 }
-Hash::Hash()
+
+void Hash :: linear_without(student s)
 {
-    for (int i = 0; i < 10; i++)
-    {
-        HashTable[i].roll_number = -1;
-        HashTable2[i].roll_number = -1;
-    }
+	int loc=s.roll_no%max;
+	if(Hashtable[loc].roll_no==-1)			//if the home address is blank
+	{
+		Hashtable[loc].roll_no=s.roll_no;
+		Hashtable[loc].name=s.name;
+		Hashtable[loc].grade=s.grade;
+		return ;
+	}
+	else				//if home address is not blank place the element in the next available blank position
+	{
+	    cout << "\nCOLLISION OCCURRED\n";
+		int i=(loc+1)%max;
+		while(i!=loc)
+		{
+			if(Hashtable[i].roll_no==-1)
+			{
+				Hashtable[i]=s;
+				return ;
+			}
+			i=(i+1)%max;
+		}
+		if(i==loc)
+			cout<<"\nHashtable is full\n";
+	}
 }
 
-void Hash::LinearProb_Without(Student Stud, int key)
+void Hash :: search(int r)
 {
-    int loc = key % 10;
-
-    if (HashTable[loc].roll_number == -1)
-    {
-        HashTable[loc].roll_number = key;
-        HashTable[loc].name = Stud.name;
-        HashTable[loc].grade = Stud.grade;
-    }
-    else
-    {
-        int j = (loc + 1) % 10;
-        while (j != loc)
-        {
-            if (HashTable[j].roll_number == -1)
-            {
-
-                HashTable[j].roll_number = key;
-                HashTable[j].name = Stud.name;
-                HashTable[j].grade = Stud.grade;
-                break;
-            }
-
-            j = (j + 1) % 10;
-        }
-        if (j == loc)
-        {
-            cout << "\nHash is Full";
-        }
-    }
+	int i;
+	int loc=r%max;
+	if(Hashtable[loc].roll_no==r)	//the element to be searched is at the home address
+	{
+		cout<<"Student Details found at location "<<loc<<"\n";
+		cout<<"Roll number :"<<Hashtable[loc].roll_no<<endl<<"Name : "<<Hashtable[loc].name<<endl<<"Grade : "<<Hashtable[loc].grade<<endl;
+	}
+	else		//else check all other locations in table unless the entry is found
+	{
+		i=(loc+1)%max;
+		while(i!=loc)
+		{
+			if(Hashtable[i].roll_no==r)
+			{
+				cout<<"Student Details found at location "<<i<<"\n";
+				cout<<"Roll number :"<<Hashtable[i].roll_no<<endl<<"Name : "<<Hashtable[i].name<<endl<<"Grade : "<<Hashtable[i].grade<<endl;
+				break;
+			}
+			i=(i+1)%max;
+		}
+	}
+	if(i==loc)
+		cout<<"Student Details not found in database\n";
 }
-void Hash::LinearProb_With(Student Stud, int key){
-    int loc = key % 10;
 
-    if (HashTable2[loc].roll_number == -1)
-    {
-        HashTable2[loc].roll_number = key;
-        HashTable2[loc].name = Stud.name;
-        HashTable2[loc].grade = Stud.grade;
-    }
-    else
-    {
-        temp=Stud;
-        int newloc=HashTable2[loc].roll_number%10;
-        if(loc!=newloc){
-            temp=HashTable2[loc];
-            HashTable2[loc].roll_number = key;
-            HashTable2[loc].name = Stud.name;
-            HashTable2[loc].grade = Stud.grade;
-
-        }
-        int j = (loc + 1) % 10;
-        while (j != loc){
-            if (HashTable2[j].roll_number == -1)
-            {
-
-                HashTable2[j].roll_number = temp.roll_number;
-                HashTable2[j].name = temp.name;
-                HashTable2[j].grade = temp.grade;
-                break;
-            }
-
-            j = (j + 1) % 10;
-        }
-        if (j == loc)
-        {
-            cout << "\nHash is Full";
-        }
-    }
-}
-void Hash::display()
-
+void Hash ::display()
 {
-
-    cout<<"Index\t\tRoll No.\tName\t\tGrade\n";
-    for (int i = 0; i < 10; i++)
-    {
-        if (HashTable[i].roll_number != -1)
-        {
-            cout<<i<<"\t\t"<<HashTable[i].roll_number<<"\t\t"<<HashTable[i].name<<"\t\t"<<HashTable[i].grade<<endl;
-        }
-    }
+	cout<<"\nThe students records are as follows:\n";
+	cout<<"\tRollNo.\t\tNAME\tGRADE\n";
+	for(int i=0;i<max;i++)
+	{
+	    cout<<i<<"\t"<<Hashtable[i].roll_no<<"\t\t"<<Hashtable[i].name<<"\t"<<Hashtable[i].grade<<"\n";
+	}
 }
-void Hash::display2()
 
+void Hash ::display_wo()
 {
-
-    cout<<"Index\t\tRoll No.\tName\t\tGrade\n";
-    for (int i = 0; i < 10; i++)
-    {
-        if (HashTable2[i].roll_number != -1)
-        {
-            cout<<i<<"\t\t"<<HashTable2[i].roll_number<<"\t\t"<<HashTable2[i].name<<"\t\t"<<HashTable2[i].grade<<endl;
-        }
-    }
+	cout<<"\nThe students records are as follows:\n";
+	cout<<"\tRollNo.\t\tNAME\tGRADE\n";
+	for(int i=0;i<max;i++)
+	{
+	    cout<<i<<"\t"<<Hashtable[i].roll_no<<"\t\t"<<Hashtable[i].name<<"\t"<<Hashtable[i].grade<<"\n";
+	}
 }
+
 
 int main()
 {
-    Hash s;
-    int c=0;
-    s.create();
+int r,c;
+char ch;
+Hash h;
     do{
-        cout<<"\n1. Displaying with Replacement";
-        cout<<"\n2. Displaying without Replacement";
-        cout<<"\n3. Exit";
-        cout<<"\n";
-        cin>>c;
-        switch(c){
-            case 1:
-                cout<<"\nDisplaying with Replacement:\n";
-                s.display2();
-                break;
-            case 2:
-                cout<<"\nDisplaying without Replacement:\n";
-                s.display();
-            case 3:
-                cout<<"\nExited";
-                break;
-        }
-
-    } while (c!=3);
+		cout<<"\nMENU\n1.Display Linear Probing Without Replacement\n2.Display Linear Probing With Replacement"<<endl;
+		cout << "3.Search Record\n4.Exit.";
+		cout << "\nEnter Your Choice: ";
+		cin >> c;
+		switch(c)
+		{
+			case 1:
+				   do
+				  {
+					student s;
+					s.accept();
+					h.linear_without(s);
+					cout<<"\nWould you like to continue(y/n): ";
+					cin>>ch;
+				  } while(ch=='y'||ch=='Y');
+				    h.display_wo();
+				    break;
+				    
+			case 2:
+				   do
+				   {
+				   student s;
+			 	   s.accept();
+				   h.linear_with(s);
+				   cout<<"\nWould you like to continue(y/n): ";
+				   cin>>ch;
+				    }while(ch=='y'||ch=='Y');
+				   h.display();
+				   break;
+				    
+			case 3:
+				   cout<<"\nEnter the roll no to be searched:";
+				   cin>>r;
+				   h.search(r);
+				   break;
+				
+			case 4:
+				   cout << "\nExiting..\n";
+				   break;
+				   
+			default:
+				cout<<"\nInvalid choice.\n";
+		}
+	}while(ch!=4);
+	return 0;
 }
